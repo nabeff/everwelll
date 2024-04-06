de078ed3596f1eec2a9c9f15e874d66c23453243
nabeff <neffina8@gmail.com>
2024-04-06 23:36:50 +0000

    frame cursor

--- src/components/frame/Frame.jsx
+++ src/components/frame/Frame.jsx
@@ -5,7 +5,7 @@ import "./frame.css"
 const Frame = () => {
   return (
     <div className='frame flex center p1 '>
-        <p>Sign up and get 20% off to your first order. <span className='bold'>Sign Up Now</span></p>
+        <p>Sign up and get 20% off to your first order. <span className='bold cursor'>Sign Up Now</span></p>
     </div>
   )
 }
